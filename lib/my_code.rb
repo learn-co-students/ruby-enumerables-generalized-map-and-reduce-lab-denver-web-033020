4ac0aa57d3195c2a7b76a5f9c27b5515f5e86948
Austin Miller <austin.miller@colorado.edu>
2020-03-01 19:04:32 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  i = 0 
+  new_array = []
+  while i < array.length do 
+  new_array << yield(array[i])
+    i += 1 
+  end 
+  new_array
+end 
+
+
+def reduce(array, start = nil)
+  if start 
+    num1 = start
+    i = 0 
+  else i = 1 
+    num1 = array[0] 
+  end 
+  while i < array.length do 
+    num1 = yield(num1, array[i])
+    i += 1 
+  end
+  num1
+end 
