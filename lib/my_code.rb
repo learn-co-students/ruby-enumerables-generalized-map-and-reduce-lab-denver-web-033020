8b38b8de7b1cd468b9df8a6fcbea87b1b0a6d4b7
Iuliia Saprykina <iuliia.proskurnina@gmail.com>
2020-03-04 06:46:52 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+
+def map(arr)
+  i = 0 
+  new_arr = []
+  while i < arr.length do 
+    new_arr.push(yield(arr[i]))
+    i += 1 
+  end 
+  return new_arr
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    result = starting_point
+    i = 0 
+  else
+    result = source_array[0]
+    i = 1 
+  end
+       while i < source_array.length do 
+         result = yield(result, source_array[i])
+         i += 1 
+     end
+  result
+end
+
+ 
+
+
