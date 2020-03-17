f7b0ed4da3bed604f89f027c624f258f70d10a4a
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-17 23:05:20 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+  #Map returns a new array of manipulated elements
+  new_array = []
+  i = 0
+  while i < array.length do
+    new_array.push(yield(array[i]))
+    i += 1
+  end
+  new_array
+end
+
+def reduce(array, sv=nil)
+  #Reduce returns a value
+  if sv
+    sum = sv
+    i = 0
+  else
+    sum = array[0]
+    i = 1
+  end
+  while i < array.length
+    sum = yield(sum, array[i])
+    i += 1
+  end
+  sum
+end
